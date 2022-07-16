552668ae19c851fc3478eba563ee4e093a6121d5
Ceyhun <contact.ceyhun@gmail.com>
2022-07-16 22:19:24 +0400

    User model which completed added again

--- user-managment/user-managment/DataBase/Models/User.cs
+++ user-managment/user-managment/DataBase/Models/User.cs
@@ -8,5 +8,25 @@ namespace user_managment.DataBase.Models
 {
     internal class User
     {
+        private static DateTime RegistrationDate = DateTime.Now;
+        private static int id = 1;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string name, string lastName, string email, string password)
+        {
+            RegistrationDate;
+            Id = id++;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
     }
 }
